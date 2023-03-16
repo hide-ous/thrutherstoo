fd72d2f23c7769a4f708d7643445866673834fc0
hide <mattia.samory@gmail.com>
2023-03-16 17:15:30 +0100

    + ccds template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='investigating the use of conspiracy theorist as an identity label',
+    author='hide',
+    license='MIT',
+)
