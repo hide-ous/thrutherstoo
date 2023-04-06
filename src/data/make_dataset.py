95ff8654f9b10609c7d78bd679b049a287d5ff67
hide <mattia.samory@gmail.com>
2023-04-06 16:17:54 +0200

    + eda

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,49 @@
 # -*- coding: utf-8 -*-
+import json
+import os.path
+import re
+from json import JSONDecodeError
+
 import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import zstandard as zstd
+import io
+
+CONSPIRACY_THEORIST_RE = '(conspiracist)|(conspiracy theorist)'
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+# @click.command()
+# @click.argument('input_filepath', type=click.Path(exists=True))
+# @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    with open(output_filepath, 'w+', encoding='utf8') as f:
+        for contribution in read_zst(input_filepath):
+            text = contribution['body']
+            if not text: continue
+            if re.findall(CONSPIRACY_THEORIST_RE, text, flags=re.I | re.DOTALL | re.U | re.M):
+                f.write(json.dumps(contribution)+'\n')
+
+
+def decompress(fh):
+    reader = zstd.ZstdDecompressor(max_window_size=2147483648).stream_reader(fh)
+    yield from io.TextIOWrapper(reader, encoding='utf-8')
+
+
+def read_zst(fpath):
+    with open(fpath, 'rb') as fh:
+        for line in decompress(fh):
+            try:
+                yield json.loads(line)
+            except JSONDecodeError as e:
+                print(f"error in {fpath}")
+                print(e)
 
 
 if __name__ == '__main__':
@@ -26,5 +56,6 @@ if __name__ == '__main__':
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
-
-    main()
+    infile = os.path.join(project_dir, 'data', 'external', 'RC_2009-09.zst')
+    outfile = os.path.join(project_dir, 'data', 'interim', 'RC_2009-09.jsonl')
+    main(input_filepath=infile, output_filepath=outfile)
---
+++ src/features/preprocess_text.py
@@ -0,0 +1,142 @@
+import re
+import string
+import warnings
+from bs4 import BeautifulSoup
+from markdown import markdown
+import spacy
+
+ESCAPE_PUNCT_RE = re.compile('[%s]' % re.escape(string.punctuation))
+
+__parser = None
+spacy_stopwords = None  # depends on the parser, should `load_spacy` before use
+
+
+# try:
+#     spacy.require_gpu()
+# except:
+#     print('no gpu support for spacy')
+
+def load_spacy(model_name='en_core_web_lg'):
+    global __parser
+    global spacy_stopwords
+    if __parser is None:
+        __parser = spacy.load(model_name)
+        spacy_stopwords = __parser.Defaults.stop_words
+        spacy_stopwords.update(set(string.punctuation))
+        # import en_core_web_lg
+        # parser = en_core_web_lg.load()
+
+
+def get_parser():
+    global __parser
+    load_spacy()
+    return __parser
+
+
+def markdown_to_text(markdown_string):
+    """ Converts a markdown string to plaintext """
+
+    # md -> html -> text since BeautifulSoup can extract text cleanly
+    html = markdown(markdown_string)
+
+    # remove code snippets
+    html = re.sub(r'<pre>(.*?)</pre>', ' ', html)
+    html = re.sub(r'<code>(.*?)</code >', ' ', html)
+
+    # extract text
+    soup = BeautifulSoup(html, "html.parser")
+    text = ' '.join(soup.findAll(text=True))
+
+    return text
+
+
+warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+
+
+def unescape_html(x):
+    return BeautifulSoup(x, features="html.parser").get_text().strip()
+
+
+def remove_urls(x):
+    return re.sub("http(.+)?(\W|$)", ' ', x)
+
+
+def normalize_spaces(x):
+    return re.sub("[\n\r\t ]+", ' ', x)
+
+
+def escape_punct(x):
+    return ESCAPE_PUNCT_RE.sub(' ', x)
+
+
+def lower(x):
+    return x.lower()
+
+
+def substitute_subreddits(x):
+    return re.sub(r"\br/", "SubredditR", x)
+
+
+def preprocess_pre_tokenizing(x):
+    return substitute_subreddits(
+        normalize_spaces(
+            remove_urls(
+                markdown_to_text(
+                    x))))
+
+
+def preprocess_e2e(x):
+    return escape_punct(
+        lower(
+            preprocess_pre_tokenizing
+            (x)))
+
+
+def doc2tokens(parsed, remove_punct=True, remove_digit=True, remove_stops=True, remove_pron=True, lemmatize=True,
+               lowercase=True):
+    tokens = list()
+    for token in parsed:
+        if remove_punct and token.is_punct:
+            continue
+        if remove_digit and token.is_digit:  # skip digits
+            continue
+        if remove_stops and (token.lemma_ in spacy_stopwords):  # skip stopwords
+            continue
+        if remove_pron and (token.lemma_ == '-PRON-'):  # skip pronouns
+            continue
+        else:
+            token = token.lemma_ if lemmatize else token.orth_
+            if lowercase:
+                token = token.lower()
+            if remove_punct:
+                token = escape_punct(token)
+
+            tokens.append(token.strip())
+    return tokens
+
+
+def text2tokens(txt, remove_punct=True, remove_digit=True, remove_stops=True, remove_pron=True, lemmatize=True,
+                lowercase=True):
+    parser = get_parser()
+    parsed = parser(preprocess_pre_tokenizing(txt))
+    return doc2tokens(parsed=parsed, remove_punct=remove_punct, remove_digit=remove_digit, remove_stops=remove_stops,
+                      remove_pron=remove_pron, lemmatize=lemmatize, lowercase=lowercase)
+
+
+def texts2tokens(txt_stream, remove_punct=True, remove_digit=True, remove_stops=True, remove_pron=True, lemmatize=True,
+                 lowercase=True, n_process=-1):
+    parser = get_parser()
+    for parsed in parser.pipe(map(preprocess_pre_tokenizing, txt_stream), n_process=n_process):
+        yield doc2tokens(parsed=parsed, remove_punct=remove_punct, remove_digit=remove_digit, remove_stops=remove_stops,
+                         remove_pron=remove_pron, lemmatize=lemmatize, lowercase=lowercase)
+
+
+def clean_items(item_stream, text_field, remove_punct=True, remove_digit=True, remove_stops=True, remove_pron=True,
+                lemmatize=True, lowercase=True, n_process=-1):
+    parser = get_parser()
+    for parsed, item in parser.pipe(((preprocess_pre_tokenizing(i[text_field]), i) for i in item_stream),
+                                    n_process=n_process, as_tuples=True):
+        item[text_field] = ' '.join(
+            doc2tokens(parsed=parsed, remove_punct=remove_punct, remove_digit=remove_digit, remove_stops=remove_stops,
+                       remove_pron=remove_pron, lemmatize=lemmatize, lowercase=lowercase))
+        yield item
