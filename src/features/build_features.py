999f4c25ba208b7d11d40010e33822acf7566eb5
hide-ous <mattia.samory@gmail.com>
2023-05-05 14:35:44 +0200

    * troubleshoot multiprocessing feature computation

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -133,8 +133,8 @@ def preprocess_files():
     out_fpath = os.path.splitext(fpath)[0] + '_preprocessed.jsonl'
     # keep only English contributions in the random sample
     with Pool(10) as pool:
-        for i in pool.map(preprocess,islice(stream_normalized_contribution(fpath), 100)):
-            print(i)
+        for i in pool.map_async(preprocess,islice(stream_normalized_contribution(fpath), 100)):
+            print(i.get())
 
         # to_file(out_fpath, map(lambda i:i.get(), ))
     # with Pool(40) as pool:
