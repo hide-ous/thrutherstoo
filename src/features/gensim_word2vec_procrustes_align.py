f3c649c188f5f523ffc70ccab07ebf5859a21eb8
hide <mattia.samory@gmail.com>
2023-05-19 22:03:18 +0200

    * working align embeddings in features

--- src/features/gensim_word2vec_procrustes_align.py
+++ src/features/gensim_word2vec_procrustes_align.py
@@ -45,6 +45,8 @@ def smart_procrustes_align_gensim(base_embed, other_embed, words=None):
                                                               words=words)
 
     # get the (normalized) embedding matrices
+    in_base_embed.wv.fill_norms(force=True)
+    in_other_embed.wv.fill_norms(force=True)
     base_vecs = in_base_embed.wv.get_normed_vectors()
     other_vecs = in_other_embed.wv.get_normed_vectors()
 
@@ -129,6 +131,7 @@ def align_years(in_dir, out_dir='../../models/embeddings/'):
                                                           year_embed)
         base_embed = aligned_embed
         print("Writing year:", year)
+        os.makedirs(out_dir, exist_ok=True)
         aligned_embed.save(
             os.path.join(out_dir,
                          f"word2vec_{year}.model"))
